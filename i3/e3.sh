f45ae8f528f5fbdccaae61f727a9ceb61d55fe7e
xircon <xirconuk@gmail.com>
2017-05-03 17:26:10 +0100

    This is a commit

---
+++ i3/e3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ~/.i3 
+joe ~/.i3/config.conf 
+i3bang config.conf 
+cp config.conf backup/config.backup.$(date +%F_%R_%S)
+i3-msg reload
+####### 
+#gitup#
+####### 
+cd ~/scripts
+git push --all -u origin 
+git add * 
+git commit -a -m 'This is a commit' 
+git push --all -u origin
+
+cd ~/.i3

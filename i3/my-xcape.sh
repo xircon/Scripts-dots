49710e5519a27177e0fcafe7931e46d55bd58ab2
xircon <xirconuk@gmail.com>
2017-05-27 08:02:41 +0100

    This is a commit

--- i3/my-xcape.sh
+++ i3/my-xcape.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 #
+############################################
+# Set both shift keys to trigger caps lock #
+############################################
+setxkbmap -option "caps:none"
+setxkbmap -option "shift:both_capslock"
+
 killall xcape
 #
 xcape -e 'Super_L=Alt_L|Tab'
-#xcape -e 'Super_R=Control_L|Alt_L|m'
 xcape -e 'Super_R=Super_L|F2'
+
+
 xcape -e 'Control_L=Super_L|1'
-xcape -e 'Alt_L=Super_L|2'
-xcape -e 'Shift_L=Escape'
-#xcape -t 200 -e 'Shift_R=Super_L|F2'
-xcape -t 200 -e 'Shift_R=Control_L|Alt_L|m'
 xcape -e 'Control_R=Control_R|Return' 
+
+xcape -e 'Alt_L=Super_L|2'
+#Reconfigure Alt_gr
+xmodmap -e "keycode 108 = Alt_R" && xset -r 108
+xcape -e 'Alt_R=Super_L|a'
+
+
+xcape -e 'Shift_L=Escape'
+xcape -t 200 -e 'Shift_R=Control_L|Alt_L|m'
+
+#############
+# Caps Lock #
+#############
+#Disable caps lock key
+xmodmap -e 'clear Lock'
+xcape -t 200 -e '#66=Alt_L|m'

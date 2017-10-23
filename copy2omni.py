fbb97146b72396bba7a9909fd29b8889ccb01c05
xircon <xirconuk@gmail.com>
2017-10-23 10:35:31 +0100

    VScode

---
+++ copy2omni.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+ 
+import pyautogui
+import time
+pyautogui.hotkey('ctrl', 'c')
+time.sleep(0.25)
+pyautogui.hotkey('ctrl', 'k')
+time.sleep(0.25)
+pyautogui.hotkey('ctrl', 'v')
+time.sleep(0.25)
+pyautogui.hotkey('enter')
